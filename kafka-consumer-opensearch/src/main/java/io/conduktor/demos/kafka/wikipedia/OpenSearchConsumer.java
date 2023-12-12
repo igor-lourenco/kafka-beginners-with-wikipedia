28b857c98df320407c9295335b919901659e53b9
ilsantos <ilsantos3@stefanini.com>
2023-12-12 14:57:52 -0300

    Criando projeto OpenSearch Consumer

---
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/wikipedia/OpenSearchConsumer.java
@@ -0,0 +1,16 @@
+package io.conduktor.demos.kafka.wikipedia;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class OpenSearchConsumer {
+    private static final Logger logger = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("I am a kafka Consumer!");
+
+        // cria instancia producer
+
+    }
+}
